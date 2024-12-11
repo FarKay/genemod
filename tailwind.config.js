d36acf58ea58101cefc7128ddb563c6353450d88
Farouk <daudafarouk35@gmail.com>
2024-12-11 16:55:25 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: "#0728b6",
+        },
+      },
+
+      fontFamily: {
+        cabin: ["Cabin", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
+
+    screens: {
+      sm: "385px",
+      md: "834px",
+      lg: "1280px",
+    },
+
+    boxShadow: {
+      "custom-1": "0px 2px 4px -2px rgba(16, 24, 40, 0.06)", // #1018280F converted to rgba
+      "custom-2": "0px 4px 8px -2px rgba(16, 24, 40, 0.10)", // #1018281A converted to rgba
+    },
+  },
+  plugins: [],
+};
